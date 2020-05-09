78cc076a5f9fed5a54b052e59afd597d52c52633
Suvigya Agrawal <suvigya1st@gmail.com>
2020-05-10 03:38:25 +0530

    Made Youtube Client generic

--- integrations_youtube/client.py
+++ integrations_youtube/client.py
@@ -1,18 +1,26 @@
-from youtube_api import YouTubeDataAPI
+from datetime import datetime
 
 from integrations_youtube.models import YoutubeCredentials
-from integrations_youtube.serializers import YoutubeDataVideoObjectSerializer
+from utils.models import Setting
+from youtube_api import YouTubeDataAPI
+
+KEY_YOUTUBE_PUBLISHED_DATE = 'YOUTUBE_PUBLISHED_DATE'
 
 
 class YoutubeClient:
     def __init__(self, credentials: YoutubeCredentials):
         self.credentials = credentials
 
-    def fetch(self, keyword, max_results=5, order_by='date'):
+    def fetch(self, keywords, published_after=None, max_results=5, order_by='date'):
         yt = YouTubeDataAPI(self.credentials.api_key)
-        response = yt.search(keyword, max_results=max_results, order_by=order_by)
-        serializer = YoutubeDataVideoObjectSerializer(data=response, many=True)
-        if serializer.is_valid():
-            serializer.save()
-        else:
-            print(serializer.errors)
+        if not published_after:
+            _published_after = Setting.objects.get(key=KEY_YOUTUBE_PUBLISHED_DATE).value
+            published_after = datetime.strptime(_published_after, '%Y-%m-%dT%H:%M:%S%z').date()
+        response = yt.search(
+            keywords,
+            max_results=max_results,
+            order_by=order_by,
+            published_after=published_after,
+        )
+        print(response)
+        return response
