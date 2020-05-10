57caa682b9d5c8962a06a9351aa51477169a1bfd
Suvigya Agrawal <suvigya1st@gmail.com>
2020-05-10 15:32:22 +0530

    Added published_before and published_after terms

--- integrations_youtube/client.py
+++ integrations_youtube/client.py
@@ -1,8 +1,10 @@
 from datetime import datetime
 
+from django.core.exceptions import ObjectDoesNotExist
+from youtube_api import YouTubeDataAPI
+
 from integrations_youtube.models import YoutubeCredentials
 from utils.models import Setting
-from youtube_api import YouTubeDataAPI
 
 KEY_YOUTUBE_PUBLISHED_DATE = 'YOUTUBE_PUBLISHED_DATE'
 
@@ -11,16 +13,22 @@ class YoutubeClient:
     def __init__(self, credentials: YoutubeCredentials):
         self.credentials = credentials
 
-    def fetch(self, keywords, published_after=None, max_results=5, order_by='date'):
+    def fetch(self, keywords, published_after=None, published_before=None, max_results=5, order_by='date'):
         yt = YouTubeDataAPI(self.credentials.api_key)
-        if not published_after:
-            _published_after = Setting.objects.get(key=KEY_YOUTUBE_PUBLISHED_DATE).value
-            published_after = datetime.strptime(_published_after, '%Y-%m-%dT%H:%M:%S%z').date()
+        if published_after:
+            _published_after = published_after.date()
+        else:
+            _published_after = datetime.datetime.timestamp(datetime.datetime(2000, 1, 1))
+        if published_before:
+            _published_before = published_before.date()
+        else:
+            _published_before = datetime.datetime.timestamp(datetime.datetime(3000, 1, 1))
         response = yt.search(
             keywords,
             max_results=max_results,
             order_by=order_by,
-            published_after=published_after,
+            published_after=_published_after,
+            published_before=_published_before,
         )
         print(response)
         return response
