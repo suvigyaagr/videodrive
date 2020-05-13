722825a5ece0ce5beeab73dfc2c0e1bb35d52542
Suvigya Agrawal <suvigya1st@gmail.com>
2020-05-13 20:59:09 +0530

    Added fields to support 24 hour quota renewal

--- integrations_youtube/models.py
+++ integrations_youtube/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime, time, timedelta
+
 from django.db import models
 
 from utils.models import BaseModel
@@ -5,7 +7,22 @@ from utils.models import BaseModel
 
 class YoutubeCredentialsManager(models.Manager):
     def get_queryset(self):
-        return super().get_queryset().filter(is_active=True)
+        final_creds = []
+        for cred in super().get_queryset().filter(is_active=True):
+            if not cred.last_expired_at:
+                final_creds.append(cred)
+            else:
+                if cred.last_expired_at < (datetime.now() - timedelta(hours=24)):
+                    final_creds.append(cred)
+                elif cred.last_expired_at.time() <= cred.quota_restarts_at <= datetime.now().time():
+                    final_creds.append(cred)
+        return final_creds
+
+    def first(self):
+        return self.get_queryset()[0]
+
+    def last(self):
+        return self.get_queryset()[-1]
 
 
 class YoutubeCredentials(BaseModel):
@@ -17,6 +34,13 @@ class YoutubeCredentials(BaseModel):
     )
     api_key = models.CharField(
         max_length=40
+    )
+    quota_restarts_at = models.TimeField(
+        default=time(0, 0, 0)
+    )
+    last_expired_at = models.DateTimeField(
+        null=True,
+        blank=True
     )
     is_active = models.BooleanField(
         default=False
